aae478465a2d6fa1ce1013b38fc7a0827c6ec261
SioSaamuri <todriabeka@gmail.com>
2023-02-10 21:20:31 +0400

    start my first project of react and import photo from local drive

---
+++ src/EquilibriumImage.js
@@ -0,0 +1,16 @@
+import equilibriumImg from "./images/image-equilibrium.jpg";
+
+export default function EquilibriumImage() {
+  return (
+    <div>
+      <img
+        style={{
+          width: "278px",
+          borderRadius: "10px",
+        }}
+        src={equilibriumImg}
+        alt="nft-pht"
+      ></img>
+    </div>
+  );
+}
